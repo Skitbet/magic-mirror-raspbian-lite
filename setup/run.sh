071876ce2ef358382a58216fcc0b16e14902782a
Skitbet <54096334+Skitbet@users.noreply.github.com>
2026-03-08 20:16:15 -0400

    Remove browser installation from run.sh
    Removed browser installation from setup script.

--- setup/run.sh
+++ setup/run.sh
@@ -5,7 +5,6 @@ SETUP_SCRIPTS_DIR=$MAGIC_MIRROR_RASP_LITE_DIR/setup/scripts
 # install required packages
 . $SETUP_SCRIPTS_DIR/install-node.sh
 . $SETUP_SCRIPTS_DIR/install-minimal-gui.sh
-. $SETUP_SCRIPTS_DIR/install-browser.sh
 
 # install magic mirror even if the user don't want to run magic mirror server
 # since we use this for splash screen
